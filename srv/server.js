25a636ecdee82d37601b12afd28e46efc4e9f694
Marlene Vasquez <marlene.vasquez@alight.com>
2024-09-13 10:51:46 +0000

    Add Authentication

--- srv/server.js
+++ srv/server.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const passport = require("passport");
+const xsenv = require("e@sap/xsenv");
+const JWTStrategy = require("@sap/xssec").JWTStrategy;
+const services = xsenv.getServices({uaa: "cftmp-xsuaa"});//xsuaa service
 const app = express();
 
-app.get("/", function(req, res, next){
+
+passport.use(new JWTStrategy(services.uaa));
+app.use(passport.initialize());
+app.use(passport.authenticate("JWT",{session:false}));
+
+/*app.get("/", function(req, res, next){
     res.send("Welcome to Basic NodeJs");
+});*/
+
+
+app.get("/", function(req, res, next){
+    res.send("Welcome to Basic NodeJs "+ req.user.id);
 });
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 app.listen(port, function(){console.log("Basic NodeJS listening on port " + port);});
